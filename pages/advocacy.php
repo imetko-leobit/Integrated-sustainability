a286f662723b789d74f045f879af02ca504bcf99
imetko-leobit <imetko@leobit.com>
2026-01-09 16:10:29 +0200

    Update advocacy page layout and styles for projects section and add academic partnerships block

--- pages/advocacy.php
+++ pages/advocacy.php
@@ -46,10 +46,16 @@
         include('../components/sections/block-section-heading.php');
     ?>
 
-    <?php
-      $text_below = true;
-      include('../components/sections/block-projects.php');
-    ?>
+    <div style="display: flex;">
+      <?php
+        $text_below = true;
+        include('../components/sections/block-projects.php');
+      ?>
+      <?php
+        $text_below = true;
+        include('../components/sections/block-projects.php');
+      ?>
+    </div>
 
     <?php
         $section_title = "translate research into operational insight";
@@ -92,6 +98,31 @@
         $link_text = null;
         $custom_icon = null;
         include('../components/sections/block-section-heading.php');
+    ?>
+
+    <?php
+      $tagline = 'academic Partnerships';
+      $title = 'Bridging research with execution to advance infrastructure intelligence';
+      $paragraphs = [
+        'We actively collaborate with universities, research councils, and innovation networks to transform applied research into operational results. These partnerships create opportunities for students, researchers, and faculty to test ideas where theory meets the field, while giving our clients access to emerging science, feasibility-grade analysis, and data-driven insights that inform project design and investment decisions.',
+      ];
+      $button = [
+        'url' => '/services',
+        'label' => 'Become a Partner'
+      ];
+      $center_item = [
+        'type' => 'image',
+        'content' => '../assets/img/directions.svg'
+      ];
+      $layout = 'image-right';
+      $checkbox_list = [
+        ['icon' => 'mark.svg', 'label' => 'Partner with academic institutions and research bodies to align emerging studies with real-world environmental and infrastructure challenges.'],
+        ['icon' => 'mark.svg', 'label' => 'Support graduate and PhD-level research connected to live projects - providing clients with feasibility-grade analysis, system optimization, and early-stage innovation scouting.'],
+        ['icon' => 'mark.svg', 'label' => 'Our experts actively contribute to academic committees, evaluate theses, and mentor student competition teams advancing applied sustainability innovation.'],
+        ['icon' => 'mark.svg', 'label' => 'We are often sought to help translate field performance into data that supports academic publication and informs regulatory and policy frameworks.'],
+      ];
+
+      include('../components/sections/block-feature.php')
     ?>
 
     <?php
