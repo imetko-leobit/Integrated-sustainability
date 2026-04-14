ec77ff93e9228182b79e8a196c6b191221b76d0d
imetko-leobit <imetko@leobit.com>
2026-04-14 22:05:44 +0300

    feat: add submenu button for Rank By filter and adjust grid layout for submenu items

--- components/elements/floating-filter-menu.php
+++ components/elements/floating-filter-menu.php
@@ -166,6 +166,11 @@
               <a href="#" class="nav-link">
                 Can't find what you're looking for? <br> Request a Tailored Credentials Package
               </a>
+              <button class="submenu-button" aria-label="Open Rank By filter">
+                <span class="arrow-icon">
+                  <svg viewBox="0 0 24 24"><path class="icon-arrow" fill="currentColor" d="M22.32 21.729 22.5 0C17.059.053 6.212.128.771.18L.725 2.901c4.897-.049 11.394-.088 17.111-.14L0 20.595 1.903 22.5 19.74 4.664l-.154 17.098 2.734-.03v-.003Z"/></svg>
+                </span>
+              </button>
             </li>
 
           </ul>
